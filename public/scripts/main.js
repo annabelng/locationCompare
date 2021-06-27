3224349e4eb2df137471f6fb4cb5c147d8b50ac0
annabelng <a.h.ng952@gmail.com>
2021-06-26 21:21:46 -0700

    first commit

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -4,12 +4,18 @@ import { createPost } from './createPost.js';
 /**
  * Driver code: initilaizes the interactables on this page
  */
+
 function init() {
 
     // TODO: First, select the form button. Then, add an event listener to it
     // that calls the createPost function when the button is clicked.
-    
-    readPosts();
+    let button = document.querySelector('input[class='button']')
+    button.addEventListener(button)
+    console.log(123)
+
+    createPost();
 }
 
 document.addEventListener('DOMContentLoaded', init);
+const myHeading = document.querySelector('h1');
+myHeading.textContent = 'Hello world!';
